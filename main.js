d0704949bd86eb3b56fc16a317f91b6ba1bcfa7a
matacat <bllackcat142@gmail.com>
2021-08-04 21:14:59 +0700

    format code

--- main.js
+++ main.js
@@ -64,7 +64,10 @@ function nextSong() {
 }
 
 function updateProgress(e) {
-  const {duration, currentTime} = e.srcElement
+  const {
+    duration,
+    currentTime
+  } = e.srcElement
   const progressPercent = (currentTime / duration) * 100
   progress.style.width = `${progressPercent}%`
 }
